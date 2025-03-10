9f4b239af3a3b5baa490a7d2d05738a2ba3e2076
Yining Hu <yining.hu@uts.edu.au>
2025-03-10 16:40:30 +1100

    adds lab8 bank project source code

---
+++ week8/lab_java/bank/Bank.java
@@ -0,0 +1,109 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Bank {
+    static DateTimeFormatter DTF = DateTimeFormatter.ofPattern("dd/MM/uuuu - HH:mm:ss");
+    static LocalDateTime NOW = LocalDateTime.now();
+    private List<Customer> customers;
+    private Manager admin;
+    private Scanner in = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        (new Bank()).menu();
+    }
+
+    public Bank() {
+        admin = new Manager();
+        customers = new ArrayList<>();
+        addCustomers();
+    }
+
+    private void addCustomers() {
+        for (int i = 0; i < 3; i++) {
+            this.customers.add(new Customer());
+        }
+    }
+
+    private char readChoice(){
+        System.out.print("Please select (L/A/X): ");
+        return in.next().charAt(0);
+    }
+    
+    private Customer customer(String name){        
+        for(Customer c:customers)
+            if(c.match(name))
+                return c;
+        return null;
+    }
+
+    private String readName() {
+        System.out.print("Enter Customer Name: ");
+        return in.next();
+    }
+
+    public void show() {
+        Customer customer = customer(readName());
+        if (customer != null)
+            System.out.println(customer);
+        else
+            System.out.println("Customer does not exist");
+    }
+    
+    public void view() {
+        customers.forEach(System.out::println);
+    }
+
+    public void add(){        
+        String name = readName();
+        Customer customer = customer(name);
+        if (customer == null) {            
+            customers.add(new Customer(name));
+        }else{
+            System.out.println("Customer already exists");
+        }
+    }
+    
+    public void remove(){
+        Customer customer = customer(readName());
+        if (customer != null){
+            customers.remove(customer);
+        }else
+            System.out.println("Customer does not exist");
+    }
+    
+    private void customerLogin() {
+        Customer c = customer(readName());
+        if (c != null)
+            c.use();
+        else
+            System.out.println("Customer does not exist");
+    }
+
+    private void adminLogin(Bank bank) {
+        admin.use(bank);
+    }
+        
+    public void menu() {
+        System.out.println("Bank menu: "+DTF.format(NOW));
+        char c;
+        while((c = readChoice()) != 'X'){
+            switch(c){
+                case 'L': customerLogin(); break;
+                case 'A': adminLogin(this); break;
+                default: help(); break;
+            }
+        }
+        System.out.println("Done");
+    }
+    
+    private void help() {
+        System.out.println("Menu options");
+        System.out.println("L = Login into customer menu");
+        System.out.println("A = Login to admin menu");
+        System.out.println("X = exit");
+    }
+}
+
---
+++ week8/lab_java/bank/Manager.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Manager{
+    private String name;
+    private Scanner in = new Scanner(System.in);
+    
+    public Manager()    {
+       this.name = "John Smith";
+    }
+
+    private void view(Bank bank){
+        bank.view();
+    }
+    
+    private void show(Bank bank){
+        bank.show();
+    }
+    
+    private void remove(Bank bank){
+        bank.remove();
+    }
+    
+    private void add(Bank bank){
+        bank.add();
+    }
+    
+    private char readChoice(){
+        System.out.print("Manager menu (a/r/s/v/x): ");
+        return in.next().charAt(0);
+    }
+
+    public void use(Bank bank) {
+        System.out.println(name+" admin menu: "+Bank.DTF.format(Bank.NOW));
+        char c;
+        while((c = readChoice()) != 'x'){
+            switch(c){
+                case 'a': add(bank); break;
+                case 'r': remove(bank); break;
+                case 's': show(bank); break;
+                case 'v': view(bank); break;
+                default: help(); break;
+            }
+        }
+        System.out.println("Back to Bank menu");
+    }
+    
+    private void help() {
+        System.out.println("Menu options");
+        System.out.println("a = add a new customer");
+        System.out.println("r = remove a customer");
+        System.out.println("s = show the selected customer statement");
+        System.out.println("v = view all customers");
+        System.out.println("x = exit");
+    }
+}
